70ea9d014eb1e8c2d47e22b9d438943042b2a06f
vladickostin <vladic.kostin@edovo.com>
2023-07-05 13:48:19 -0500

    Get API key test

--- src/Edovo.js
+++ src/Edovo.js
@@ -1,7 +1,19 @@
+import axios from "axios";
+
+const URL = 'https://edovo-gpt-3d22912cfd6d.herokuapp.com'
+
 const Edovo = () => {
+
+    const onClick = async () => {
+        const data = await axios.get(`${URL}/api/api_key`);
+        console.log(data);
+    }
+
+
     return (
         <div>
             <h1>HELLO EDOVO</h1>
+            <button onClick={onClick}>GET API KEY</button>
         </div>
     )
 }
