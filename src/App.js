a7b2480f1113d8dc7f8b442553696e5f939fc67b
vladickostin <vladic.kostin@edovo.com>
2023-07-05 13:31:19 -0500

    Edovo

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React, { useState } from 'react';
 import AppBar from './components/AppBar';
 import AppMain from './components/AppMain';
 import GameContextProvider from './context/GameContext'
+import Edovo from './Edovo';
 
 function App() {
   const [isPlaying, setIsPlaying] = useState(false)
 
   const toggleScreen = () => setIsPlaying(prevState => !prevState)
 
-  return (
-    <GameContextProvider>
-      <>
-        <AppBar goBack={toggleScreen} isPlaying={isPlaying} />
-        <AppMain startGame={toggleScreen} isPlaying={isPlaying} />
-      </>
-    </GameContextProvider>
-  );
+  return <Edovo/>
+
+  // return (
+  //   <GameContextProvider>
+  //     <>
+  //       <AppBar goBack={toggleScreen} isPlaying={isPlaying} />
+  //       <AppMain startGame={toggleScreen} isPlaying={isPlaying} />
+  //     </>
+  //   </GameContextProvider>
+  // );
 }
 
 export default App;
---
+++ src/Edovo.js
@@ -0,0 +1,9 @@
+const Edovo = () => {
+    return (
+        <div>
+            <h1>HELLO EDOVO</h1>
+        </div>
+    )
+}
+
+export default Edovo;
