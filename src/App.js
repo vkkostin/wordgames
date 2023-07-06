9c36b0c089c93a82f264b1ee3f86db724dbb89cf
vladickostin <vladic.kostin@edovo.com>
2023-07-05 23:41:36 -0500

    Undo stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-// import AppBar from './components/AppBar';
-// import AppMain from './components/AppMain';
-// import GameContextProvider from './context/GameContext'
-import Edovo from './Edovo';
+import AppBar from './components/AppBar';
+import AppMain from './components/AppMain';
+import GameContextProvider from './context/GameContext'
 
 function App() {
-  // const [isPlaying, setIsPlaying] = useState(false)
+  const [isPlaying, setIsPlaying] = useState(false)
 
-  // const toggleScreen = () => setIsPlaying(prevState => !prevState)
+  const toggleScreen = () => setIsPlaying(prevState => !prevState)
 
-  return <Edovo/>
-
-  // return (
-  //   <GameContextProvider>
-  //     <>
-  //       <AppBar goBack={toggleScreen} isPlaying={isPlaying} />
-  //       <AppMain startGame={toggleScreen} isPlaying={isPlaying} />
-  //     </>
-  //   </GameContextProvider>
-  // );
+  return (
+    <GameContextProvider>
+      <>
+        <AppBar goBack={toggleScreen} isPlaying={isPlaying} />
+        <AppMain startGame={toggleScreen} isPlaying={isPlaying} />
+      </>
+    </GameContextProvider>
+  );
 }
 
 export default App;
